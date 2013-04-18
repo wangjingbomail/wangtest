2ada3c57bc8b5d257820c302382de6390c8b806b
wangjingbomail@163.com <wangjingbo@wangjingbo>
2013-04-18 14:56:51 +0800

    测试例子

--- WangTest/src/com/wang/asmtest/ChangeVersionAdapter.java
+++ WangTest/src/com/wang/asmtest/ChangeVersionAdapter.java
@@ -1,7 +1,9 @@
 
 package com.wang.asmtest;
 
+import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 
 public class ChangeVersionAdapter extends ClassVisitor {
@@ -16,7 +18,37 @@ public class ChangeVersionAdapter extends ClassVisitor {
 	}
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		test2();
+
+	}
+	
+	public static void test1() throws Exception {
+		ClassReader cr = new ClassReader("com.wang.asmtest.Output");
+		ClassWriter cw = new ClassWriter(cr, 0);
+		ChangeVersionAdapter adapter = new ChangeVersionAdapter(cw);
+		cr.accept(adapter, 0);
+		byte[] b = cw.toByteArray();
+		
+		Output.write(b, "Output");		
+	}
+	
+	public static void test2() throws Exception {
+		
+		
+		byte[] b1 = ReadClass.read("com.wang.asmtest.Output");
+		ClassWriter cw = new ClassWriter(0);
+		
+		ChangeVersionAdapter ca = new ChangeVersionAdapter(cw);
+		
+		ClassReader cr = new ClassReader("com.wang.asmtest.Output");
+		cr.accept(ca, 0);
+		
+		byte[] b2 = cw.toByteArray();
+		
+		Output.write(b2, "Output2");
+		
+		
 		
 	}
 }
