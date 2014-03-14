e88ece471f706cb03aff411f24cb31d935f2dbf9
jingbo7 <jingbo7@JINGBO7.staff.sina.com.cn>
2014-03-14 14:36:11 +0800

    增加spring的包

--- WangTest/src/com/wang/dao/SpringJDBCTemplate.java
+++ WangTest/src/com/wang/dao/SpringJDBCTemplate.java
@@ -1,6 +1,10 @@
 package com.wang.dao;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 
 /**
